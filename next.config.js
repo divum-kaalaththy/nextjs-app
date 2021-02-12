a45da8b469d2649a4ddc7388dac6b36cfbe82531
divum-kaalaththy <kaalaththy@divum.in>
2021-02-12 16:09:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,1 @@
-module.exports = {
-  env: {
-    AWS_ACCESS_KEY_ID: 'AKIA4VKJGHRCVKEUR6IU',
-  },
-  serverRuntimeConfig: {
-    AWS_SECRET_ACCESS_KEY: 'aPsyeH4O8kWUSrOjs087Vr8OFnt0ptx7LBczK+qc',
-  },
-};
+
